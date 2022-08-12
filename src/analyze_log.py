4ff92fae9a297f4f22138c555f0fda68cd598e8a
Diego Silva <coxcorp@gmail.com>
2022-08-12 20:12:30 -0300

    requisito 01

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,42 @@
+import csv
+from src.track_orders import TrackOrders
+
+
+def read_file(path_to_file):
+    with open(path_to_file, "r") as raw:
+        return [row for row in csv.reader(raw)]
+
+
+def create_data(orders):
+    track_orders = TrackOrders()
+    for customer, order, day in orders:
+        track_orders.add_new_order(customer, order, day)
+    return track_orders
+
+
+def analyze_report(orders):
+    track_orders = create_data(orders)
+    maria_eats = track_orders.get_most_ordered_dish_per_customer("maria")
+    arnaldo_ask_hamburguer = track_orders.asked_by_customer(
+        "arnaldo", "hamburguer"
+    )
+    joao_never_ask = track_orders.get_never_ordered_per_customer("joao")
+    joao_never_went = track_orders.get_days_never_visited_per_customer("joao")
+
+    with open("data/mkt_campaign.txt", "w") as file:
+        file.write(
+            f"{maria_eats}\n"
+            f"{arnaldo_ask_hamburguer}\n"
+            f"{joao_never_ask}\n"
+            f"{joao_never_went}\n"
+        )
+
+
 def analyze_log(path_to_file):
-    pass
+    if not (".csv") in path_to_file:
+        raise FileNotFoundError(f"Extensão inválida: {path_to_file}")
+    try:
+        orders = read_file(path_to_file)
+        analyze_report(orders)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: {path_to_file}")
--- src/track_orders.py
+++ src/track_orders.py
@@ -15,18 +15,27 @@ class TrackOrders:
         self._menu.add(order)
         self._days.add(day)
 
-    def get_most_ordered_dish_per_customer(self, customer):
+    def customer_order(self, customer):
         result = list()
         for order in self._data:
             if customer in order["customer"]:
                 result.append(order["order"])
+        return result
+
+    def asked_by_customer(self, customer, asked):
+        result = list()
+        for order in self._data:
+            if customer in order["customer"]:
+                if asked in order["order"]:
+                    result.append(order)
+        return len(result)
+
+    def get_most_ordered_dish_per_customer(self, customer):
+        result = self.customer_order(customer)
         return Counter(result).most_common(1)[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        result = list()
-        for order in self._data:
-            if customer in order["customer"]:
-                result.append(order["order"])
+        result = self.customer_order(customer)
         return self._menu.difference(result)
 
     def get_days_never_visited_per_customer(self, customer):
@@ -36,16 +45,16 @@ class TrackOrders:
                 result.append(order["day"])
         return self._days.difference(result)
 
-    def get_busiest_day(self):
+    def get_days(self):
         result = list()
         for order in self._data:
             result.append(order["day"])
+        return result
 
+    def get_busiest_day(self):
+        result = self.get_days()
         return Counter(result).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        result = list()
-        for order in self._data:
-            result.append(order["day"])
-
+        result = self.get_days()
         return Counter(result).most_common()[-1][0]
